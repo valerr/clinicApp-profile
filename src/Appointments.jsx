1d3e9094f76af6f72659edb93306238c9b40f10a
valerr <spokich394@gmail.com>
2020-10-03 13:29:54 +0300

    add basic project template

---
+++ src/Appointments.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const Appointments = () => {
+
+
+    return (
+        <div>
+            <h2>Записи на прием</h2>
+
+        </div>
+    )
+}
+
+export default Appointments;
---
+++ src/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import Appointments from './Appointments';
+
+const Navbar = () => {
+    const [currentMenu, setCurrentMenu] = useState('profile');
+return (
+<>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light-blue border-bottom">
+          <h2 class="text-white">Мой профиль</h2>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto mt-lg-0">
+              <li class="nav-item pt-2">
+                <a class="nav-link" href="#"><img alt="profile" src="./images/search.svg"/></a>
+              </li>
+              <li class="nav-item pt-2">
+                <a class="nav-link" href="#"><img alt="profile" src="./images/notifications.svg"/></a>
+              </li>
+              <li class="nav-item pt-2">
+                <a class="nav-link" href="#"><img alt="profile" src="./images/eye.svg"/></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><img alt="profile" src="./images/profilePhoto.png"/></a>
+              </li>
+              <li class="nav-item dropdown pt-2">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <img alt="profile" src="./images/dropdown.svg"/>
+                  Dropdown
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+  
+        <div class="container-fluid" id="main-content">
+        { currentMenu === 'profile' ? <Appointments /> : null }
+      </div>
+      
+  
+    </>
+    );
+}
+
+    export default Navbar;
